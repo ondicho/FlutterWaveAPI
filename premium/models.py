99ba5e5ce79e00e92f104f3d5aeec28e23e70a8e
ondicho <ondicho53@gmail.com>
2021-12-17 01:11:03 +0300

    creates payment and currency model

--- premium/models.py
+++ premium/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Payment(models.Model):
+    payment_choices = [
+        ("account", "account"),
+        ("card", "card"),
+        ("banktransfer", "banktransfer"),
+        ("mpesa", "mpesa"),
+        ("mobilemoneyrwanda", "mobilemoneyrwanda"),
+        ("mobilemoneyzambia", "mobilemoneyzambia"),
+        ("qr", "qr"),
+        ("mobilemoneyuganda", "mobilemoneyuganda"),
+        ("ussd", "ussd"),
+        ("credit", "credit"),
+        ("barter", "barter"),
+        ("mobilemoneyghana", "mobilemoneyghana"),
+        ("payattitude", "payattitude"),
+        ("mobilemoneyfranco", "mobilemoneyfranco"),
+        ("paga", "paga"),
+        ("1voucher", "1voucher"),
+        ("mobilemoneytanzania", "mobilemoneytanzania")
+    ]
+    currency_choices = [('KES', 'KES'), ('USD', 'USD')]
+    trx_ref = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    currency = models.ForeignKey('Currency',on_delete=models.PROTECT)
+    amount = models.IntegerField(blank=True, null=True)
+    phoneNumber = models.BigIntegerField()
+    payment_options = models.CharField(choices=payment_choices, max_length=32)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Currency(models.Model):
+    id = models.IntegerField(primary_key=True, null=False)
+    code = models.CharField(max_length=3)
--- premium/views.py
+++ premium/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+# def Subscriptionpayment(request):
+#     if request.method == 'POST':
